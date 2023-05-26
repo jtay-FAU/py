fe7c165757bfbe75451245da64aefa543fb98615
jtay-FAU <57227522+jtay-FAU@users.noreply.github.com>
2023-05-26 09:07:38 +0100

    faq_app.py

--- faq_app.py
+++ faq_app.py
@@ -87,7 +87,7 @@ def main():
         # Styling for FAQs
         question_color = "#3399ff"  # Blue
         question_style = f"background-color: {question_color}; padding: 10px; color: white; font-weight: bold; border-radius: 5px;"
-        answer_style = "padding: 10px;"
+        answer_style = "padding: 10px; margin-top: 10px; border-radius: 5px;"
 
         for faq in generate_faqs(text):
             if faq.startswith("Q:"):
@@ -95,7 +95,7 @@ def main():
                 st.markdown(f"<div style='{question_style}'>Q: <b>{question}</b></div>", unsafe_allow_html=True)
             elif faq.startswith("A:"):
                 answer = faq[3:].strip()  # Extract answer text without 'A: '
-                st.markdown(f"<div style='{answer_style}'><p>A: {answer}</p></div>", unsafe_allow_html=True)
+                st.markdown(f"<div style='{answer_style}'>A: {answer}</div>", unsafe_allow_html=True)
             else:
                 st.markdown(f"<div>{faq}</div>")
 
